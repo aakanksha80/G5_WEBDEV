b5d4b16f38ee82342703f577e7f1eb7d0427144f
aakanksha80 <as4704312@gmail.com>
2025-07-10 10:44:43 +0530

    Event Delegation Done

---
+++ L7_Event_delegation/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+const listOfProducts=document.getElementById("listOfProducts")
+// listOfProducts.addEventListener("click",(event)=>{
+//     if(event.target.tagName==="LI"){
+//         console.log("CLicked on:",event.target.textContent);
+//     }
+// })
+// const newProduct=document.createElement("li")
+// newProduct.textContent="Mobile Phone"
+// listOfProducts.appendChild(newProduct);
