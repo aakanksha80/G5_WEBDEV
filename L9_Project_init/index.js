6061c814045d8e3e6458c9a35a2ce327ed1b6aef
aakanksha80 <as4704312@gmail.com>
2025-07-15 10:54:28 +0530

    Project init Added

---
+++ L9_Project_init/index.js
@@ -0,0 +1,54 @@
+// const{divOfTwo,xy}=require("./divOfTwo");
+// const sumOfTwo=require("./sumOfTwo");
+// // function sumOfTwo(a,b){
+// //     console.log(a+b)   
+// //     //230 lines
+// //     return a+b;
+// // }
+// // function divOfTwo(a,b){
+// //     console.log(a/b)   
+// //     //230 lines
+// //     return a/b;
+// // }
+// // function mulOfTwo(a,b){
+// //     console.log(a*b)   
+// //     //230 lines
+// //     return a*b;
+// // }
+// // function subOfTwo(a,b){
+// //     console.log(a-b)   
+// //     //230 lines
+// //     return a-b;
+// // }
+// sumOfTwo(10,20);
+// divOfTwo(30,40)
+// console.log(xy);
+// // mulOfTwo(30,10);
+
+
+var a=2;
+var b=30;
+function call(){
+    console.log("call function");
+    
+}
+console.log(a);
+call();
+console.log(b);
+
+setTimeout(()=>{
+    console.log("This is setTimeOut");
+    
+},2000)
+setTimeout(()=>{
+    console.log("This is setTimeOut1");
+    
+},2000)
+setTimeout(()=>{
+    console.log("This is setTimeOut2");
+    
+},4000)
+setTimeout(()=>{
+    console.log("This is setTimeOut3");
+    
+},3000)
